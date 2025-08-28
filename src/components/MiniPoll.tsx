5321b3391d92df6f1f98c1dd87ef6ea39a1870eb
Chaim Shteinberg <a0533197133@gmail.com>
2025-08-28 12:39:05 +0300

    keep votes object

--- src/components/MiniPoll.tsx
+++ src/components/MiniPoll.tsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 
 function MiniPoll() {
   const [options, setOptions] = useState(["React", "Vue", "Svelte"]);
+  const [votes, setVotes] = useState({
+    React: 0,
+    Vue: 0,
+    Svelte: 0,
+  });
   return <></>;
 }
 
